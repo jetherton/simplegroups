17724fd7ffaf36b1a2a7d7c8ac84c63087db9493
jetherton <john.etherton@gmail.com>
2010-12-22 19:10:07 -0700

    Made it so you can have a name and org attached to each white listed number. You can also see that name and org when looking at messages and reports that originated from that number

---
+++ views/simplegroups/number_info_message.php
@@ -0,0 +1,5 @@
+<div style="color:#9b0000; font-size:10pt;">
+<?php
+echo "<strong>Number belongs to:</strong> $name -- $org";
+?>
+</div>
---
+++ views/simplegroups/number_info_report.php
@@ -0,0 +1,8 @@
+
+
+<div class="row">
+	<h4>Reporter</h4>
+	<span style="font-size:11pt;">
+		This report was created by a message sent in from: <strong> <?php echo "$name -- $org"; ?> </strong>
+	</span>
+</div>
