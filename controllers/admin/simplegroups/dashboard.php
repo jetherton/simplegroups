af807f99dcc82718cd06d9e785bc5569ff5def57
jetherton <john.etherton@gmail.com>
2011-01-10 20:31:56 -0700

    The way I was counting messages for the dashboard was wrong. Fixed it.

--- controllers/admin/simplegroups/dashboard.php
+++ controllers/admin/simplegroups/dashboard.php
@@ -41,8 +41,9 @@ class Dashboard_Controller extends Admin_simplegroup_Controller
         $this->template->content->categories = ORM::factory('category')->count_all();
 
         // Messages By Service
-	$this->template->content->message_count = ORM::factory('simplegroups_groups_message')
-		->where('simplegroups_groups_id', $this->group->id)
+	$this->template->content->message_count = ORM::factory('message')
+		->join("simplegroups_groups_message", "simplegroups_groups_message.message_id", "message.id")
+		->where('simplegroups_groups_message.simplegroups_groups_id', $this->group->id)
 		->count_all();
 
 
