12b4b32ae71e4bf2e6cc82af5ee0cca95c11bd2a
jetherton <john.etherton@gmail.com>
2011-01-31 14:24:09 -0700

    I setup the forward to link wrong and fixed it

--- views/simplegroups/forwardto.php
+++ views/simplegroups/forwardto.php
@@ -39,5 +39,5 @@ function forwardMessage<?php echo $message_id; ?>()
 ?>
 Forward To:
 <?php print form::dropdown("input_forwardto_".$message_id, $groups_array, 'standard'); ?>
-<a href="javascript:forwardMessage<?php echo $message_id; ?>()"  style="border: #d1d1d1 1px solid; background-color:#F2F7Fa; color: #5c5c5c; padding: 0px 9px; line-height:24px; text-decoration:none;">Forward</a>
+<a href="#" onclick="javascript:forwardMessage<?php echo $message_id; ?>(); return false;"  style="border: #d1d1d1 1px solid; background-color:#F2F7Fa; color: #5c5c5c; padding: 0px 9px; line-height:24px; text-decoration:none;">Forward</a>
 <span id="forwarded_<?php echo $message_id; ?>"style="width: 70px; display:none; color:#555; background-color:#d8f1d8; border: 2px solid #a7d1a7;">Forwarded</span>
