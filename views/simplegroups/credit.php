e3dc05010aa8c95b4f8bbbe948f075e86bc7cec3
jetherton <john.etherton@gmail.com>
2011-04-14 13:21:18 -0600

    made it so credit shows up nicely in the new feel of the site

--- views/simplegroups/credit.php
+++ views/simplegroups/credit.php
@@ -1,14 +1,15 @@
 <?php $group_page = url::site()."simplegroups/index/".$group_id; ?>
-<li>
-<small>Group</small>
-<br/>
-This report created by: <strong><a href="<?php echo $group_page; ?>"> <?php echo $group_name; ?></a></strong>
-<br/>
-<?php
-                        $thumb = $logo_file."_t.jpg";
-                        $prefix = url::base().Kohana::config('upload.relative_directory')."/groups";
-	                        print "<a href='$group_page'>";
-	                        print "<img src=\"$prefix/$thumb\" >";
-	                        print "</a>";
+
+	<br/>
+	<br/>
+	<h5 style="font-size:14px;border-top:1px dotted #c0c2b8;">Group</h5>
+	
+	This report created by: <a style="float:none;font-size:14px;" href="<?php echo $group_page; ?>"> <?php echo $group_name; ?></a>
+	<br/>
+	<?php
+		$thumb = $logo_file."_t.jpg";
+		$prefix = url::base().Kohana::config('upload.relative_directory')."/groups";
+			print "<a href='$group_page'>";
+			print "<img src=\"$prefix/$thumb\" >";
+			print "</a>";
 	?>
-</li>
