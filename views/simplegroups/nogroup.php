b54018c33fac21fc2f28df019d27e5df92411d4d
John Etherton <john.etherton@gmail.com>
2011-05-26 11:01:34 -0600

    Fixed another link that pointed ot the wrong thing

--- views/simplegroups/nogroup.php
+++ views/simplegroups/nogroup.php
@@ -12,7 +12,7 @@
 	<h1>Error: No Group Assigned</h1> 
 	We are sorry, but it seems that your user account has not been assigned to a group. <br/><br/> Please contact this web site's administrator to correct this. 
 	<br/><br/>
-	If you have another use account you can <a href="<?php echo url::site()."admin/";?>log_out"><?php echo Kohana::lang('ui_admin.logout');?></a> and then log back in using that user account.
+	If you have another use account you can <a href="<?php echo url::site();?>logout"><?php echo Kohana::lang('ui_admin.logout');?></a> and then log back in using that user account.
 	<br/><br/> Thank you.
 </div> 
 </body> 
